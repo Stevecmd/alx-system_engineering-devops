d850a55fa5b2237d3c8a1d0fba3a9884a232426b
Stevecmd <murimi101@gmail.com>
2024-06-28 18:48:42 +0300

    minor update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,14 +7,9 @@
 # - Install Flask
 # - Flask version must be 2.1.0
 #
-# Usage:
-# 1. Save this file as 1-install_a_package.pp
-# 2. Run `puppet-lint 1-install_a_package.pp` to check for syntax issues.
-# 3. Apply the manifest with `puppet apply 1-install_a_package.pp`.
-# 4. Verify the installation with `flask --version`.
 
 # Define the package resource
-package {'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
