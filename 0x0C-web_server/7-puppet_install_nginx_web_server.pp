fe377eb589b245bf04a90ae667df44f882c3c7d8
Stevecmd <murimi101@gmail.com>
2024-07-03 12:49:10 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,48 +9,22 @@ exec { 'update system':
 # Install Nginx
 package { 'nginx':
   ensure  => installed,
-  require => Exec['update system'],
+  require => Exec['update system']
 }
 
 # Create the index.html file with "Hello World!"
 file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['nginx'],
+  content => 'Hello World!'
 }
 
-# Directly configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @(EOF)
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 http://example.com;
-    }
-}
-  | EOF
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+# Configure the Redirect page
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+# Configure Nginx
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
