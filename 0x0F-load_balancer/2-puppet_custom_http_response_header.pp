c4aeeceaeb9ca84b46ddf832e94ff04b48040677
Stevecmd <murimi101@gmail.com>
2024-07-09 04:07:22 +0300

    test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,15 @@
-# Puppet script to configure a new Ubuntu server with Nginx and a custom HTTP header
+# Puppet script to configure a new Ubuntu server with Nginx, a custom HTTP header, and system update
 
-# Install nginx package
+# Update system package list
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+  path    => ['/bin/', '/usr/bin/'],
+}
+
+# Install nginx package with system update requirement
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['update system'],
 }
 
 # Ensure the necessary directories exist
@@ -31,35 +38,26 @@ file { '/var/www/html/404.html':
   mode    => '0644',
 }
 
-# Configure Nginx directly in the manifest
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @("EOF"),
-server {
-    listen      80;
-    listen      [::]:80;
-    add_header  X-Served-By $hostname;
-    root        /var/www/html;
-    index       index.html index.htm;
-
-    location /redirect_me {
-        return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/html;
-        internal;
-    }
+# Add redirect rule using exec resource
+exec { 'redirect_me':
+  command     => 'sed -i "24i\\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  path        => ['/bin/', '/usr/bin/'],
+  refreshonly => true,
+  subscribe   => Package['nginx'],
 }
-| EOF
-  notify  => Service['nginx'],
-  mode    => '0644',
+
+# Add custom HTTP header using exec resource
+exec { 'HTTP header':
+  command     => 'sed -i "25i\\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  path        => ['/bin/', '/usr/bin/'],
+  refreshonly => true,
+  subscribe   => Package['nginx'],
 }
 
 # Ensure nginx service is running and enabled
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe  => File['/etc/nginx/sites-available/default'],
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => [Exec['redirect_me'], Exec['HTTP header']],
 }
