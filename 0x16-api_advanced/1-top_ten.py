110301bdc07605b2349b1ea7b42a075ac1919392
SteveCmd <murimi101@gmail.com>
2024-08-13 13:48:35 +0300

    feat: Add function to retrieve top 10 hot posts from a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed on a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print(None)
+            return
+        data = response.json().get("data", {}).get("children", [])
+        if not data:
+            print(None)
+            return
+        for post in data:
+            print(post.get("data", {}).get("title"))
+    except requests.RequestException:
+        print(None)
