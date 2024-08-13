7a2cf6bed321b7caf04991f497b9ee774577802c
SteveCmd <murimi101@gmail.com>
2024-08-13 14:39:06 +0300

    refactor: Update top_ten function to include subreddit in User-Agent header, added external params

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,13 +9,21 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed on a subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+    params = {
+        "limit": 10
+    }
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+        )
         if response.status_code != 200:
             print(None)
             return
