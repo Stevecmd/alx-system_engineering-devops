b7f3fa7af82c8a47496130025558880a6dbefe4d
SteveCmd <murimi101@gmail.com>
2024-08-13 14:26:13 +0300

    refactor: Improve error handling in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,8 +17,9 @@ def number_of_subscribers(subreddit):
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            data = response.json().get("data")
-            return data.get("subscribers", 0)
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
     except requests.RequestException:
         pass
     return 0
