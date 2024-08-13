d2744b22a5e042b69aa69c00e625da24f762b6ce
SteveCmd <murimi101@gmail.com>
2024-08-13 13:25:53 +0300

    feat: Update User-Agent header in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,12 @@ import requests
     and returns the number of subscribers.
 """
 
+
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and
        returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
