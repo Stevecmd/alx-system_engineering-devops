0af6d2932c20c8827780ce24d749f6e34bad1873
SteveCmd <murimi101@gmail.com>
2024-08-13 22:44:37 +0300

    undo last commit, no improvement to checkers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,18 +8,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
+        if response.status_code == 404:
+            return 0
+        data = response.json().get("data")
+        return data.get("subscribers", 0)
     except requests.RequestException:
-        pass
-    return 0
+        return 0
